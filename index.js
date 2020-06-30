522b6277737191a7c704b365fd17a0bffe4e4c9b
Dan Burzo <dan@danburzo.ro>
2020-06-30 14:39:03 +0300

    move index.js to root

---
+++ index.js
@@ -0,0 +1,2 @@
+export { default as stackingCtx } from '../stacking-ctx.js';
+export { highlight } from '../util.js';
--- src/index.js
+++
@@ -1,2 +0,0 @@
-export { default as stackingCtx } from './stacking-ctx.js';
-export { highlight } from './util.js';
